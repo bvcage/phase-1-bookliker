7a8cca1cfbfb3f2deb4a6a56a36af7994b6ae276
Bailey Cage <bailey.cage@gmail.com>
2022-08-16 19:48:38 -0400

    add like button and related functionality

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+var USER;
+const USER_ID = Math.floor( Math.random() * 10 ) + 1;
+console.log('USER_ID = ' + USER_ID);
+fetch(`http://localhost:3000/users/${USER_ID}`)
+.then((response) => response.json())
+.then((data) => {
+    USER = data;
+});
+
 window.addEventListener("DOMContentLoaded", function() {
 
     const bookList = document.querySelector('ul#list');
@@ -55,17 +64,57 @@ window.addEventListener("DOMContentLoaded", function() {
             bookDetailPanel.append(bookDesc);
         }
         if (users) {
+            const bookLikeList = makeUserLikeList(users);
+            bookDetailPanel.append(bookLikeList);
+        }
+
+        function makeUserLikeList (users) {
             const bookLikeList = document.createElement('ul');
             users.forEach((user) => {
                 const bookLike = document.createElement('li');
                 bookLike.textContent = user.username;
                 bookLikeList.append(bookLike);
             });
-            bookDetailPanel.append(bookLikeList);
+            return bookLikeList;
         }
 
-    }
+        function appendLikeButton () {
+            const likeBtn = document.createElement('button');
+            likeBtn.textContent = 'LIKE';
+            bookDetailPanel.append(likeBtn);
+            likeBtn.addEventListener('click', updateUserLikes);
+        }
 
+        function updateUserLikes () {
+            // check if user already liked
+            if (users.find(user => user.id === USER_ID)) {
+                console.log(USER_ID + " has already liked this book.");
+            } else {
+                users.push(USER);
+                const updatedLikes = {
+                    "users": users
+                }
+                const patchUserLikes = {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-type': 'application/json',
+                        'Accept': 'application/json'
+                    },
+                    body: JSON.stringify(updatedLikes)
+                }
+                fetch(`http://localhost:3000/books/${book.id}`, patchUserLikes)
+                .then(response => response.json())
+                .then(updatedBook => {
+                    const oldLikeList = bookDetailPanel.querySelector('ul');
+                    const newLikeList = makeUserLikeList(updatedBook.users);
+                    bookDetailPanel.replaceChild(makeUserLikeList(updatedBook.users), oldLikeList);
+                });
+            }
+        }
+
+        appendLikeButton();
+
+    }
 
 });
 
