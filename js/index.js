af807b98a8fe38055a525724cc1e50e16c435422
Bailey Cage <bailey.cage@gmail.com>
2022-08-16 18:03:59 -0400

    fetch books from db, display in DOM

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {});
+window.addEventListener("DOMContentLoaded", function() {
+
+    fetch('http://localhost:3000/books')
+    .then((response) => response.json())
+    .then((data) => {
+        listBooks(data);
+    })
+    .catch((error) => console.log(error));
+    
+});
+
+function listBooks (inventory) {
+    const bookList = document.querySelector('ul#list');
+    Object.values(inventory).forEach((book) => {
+        const newBookLine = document.createElement('li');
+        newBookLine.textContent = book.title;
+        bookList.appendChild(newBookLine);
+        newBookLine.addEventListener('click', showDetails);
+    });
+}
+
+
+
+function showDetails () {
+    console.log(event.target);
+}
